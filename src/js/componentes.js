338ad3824fe7e556531b5fd2a85537a8ead38f6f
Emmanuel Guadalupe <ema_110@outlook.es>
2022-06-25 22:25:37 -0500

    Importación de estilos de forma dinámica

--- src/js/componentes.js
+++ src/js/componentes.js
@@ -1,3 +1,5 @@
+import '../css/componentes.css'
+
 export const saludar = (nombre) => {
   console.log('Creando h1');
   const h1 = document.createElement('h1');
--- webpack.config.js
+++ webpack.config.js
@@ -16,6 +16,10 @@ module.exports = {
           sources: false,
         },
       },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
     ],
   },
   
