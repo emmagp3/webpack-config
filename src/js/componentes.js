f2d6e5f50332601a8c6f2b581cebde65d87373e7
Emmanuel Guadalupe <ema_110@outlook.es>
2022-07-04 22:07:39 -0500

    Manejo de imágenes y otros archivos con 'CopyWebpackPlugin'

--- src/js/componentes.js
+++ src/js/componentes.js
@@ -1,5 +1,5 @@
 import '../css/componentes.css';
-import webpackLogo from '../assets/img/webpack-logo.png';
+// import webpackLogo from '../assets/img/webpack-logo.png';
 
 export const saludar = (nombre) => {
   console.log('Creando h1');
@@ -7,7 +7,7 @@ export const saludar = (nombre) => {
   h1.innerText = `¡Hola, ${nombre}!`;
   document.body.append(h1);
 
-  const img = document.createElement('img');
-  img.src = webpackLogo;
-  document.body.append(img);
+  // const img = document.createElement('img');
+  // img.src = webpackLogo;
+  // document.body.append(img);
 }
